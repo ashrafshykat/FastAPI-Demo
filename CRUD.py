8f03d01d275b1e81f89d1275fb0695d2f4e5fbe8
Ashraf Sharif <98658799+ashrafshykat@users.noreply.github.com>
2025-12-31 15:33:38 +0600

    implemented query

--- CRUD.py
+++ CRUD.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -28,9 +28,25 @@ def view():
     return data
 
 @app.get('/products/{id}')
-def viewprod(id: int):
+def viewprod(id: int = Path(..., description='enter integer value to get product')):
     data = load_data()
     for product in data:
         if product['id'] == id:
             return product
     raise HTTPException(status_code=404, detail='product not found')
+
+@app.get("/query")
+def sort_products(
+    sort_by: str = Query("id", enum=["id", "name", "price"]),
+    order: str = Query("asc", enum=["asc", "desc"])
+):
+    data = load_data()
+
+    reverse = True if order == "desc" else False
+
+    try:
+        sorted_data = sorted(data, key=lambda x: x[sort_by], reverse=reverse)
+    except KeyError:
+        raise HTTPException(status_code=400, detail="Invalid sort field")
+
+    return sorted_data
