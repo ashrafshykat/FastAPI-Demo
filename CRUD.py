cadf330e55f6021861ef81dce56a36847f505e31
Ashraf Sharif <98658799+ashrafshykat@users.noreply.github.com>
2025-12-31 15:16:55 +0600

    products/{id} working

--- CRUD.py
+++ CRUD.py
@@ -1,27 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
 
 def load_data():
-    with open('product.json', r) as f:
+    with open('products.json', 'r') as f:
         data = json.load(f)
     
     return data
 
 
-@app.get("/")
+@app.get('/')
 def hello():
     return {'message': 'Hello'}
 
-@app.get("/about")
+@app.get('/about')
 def about():
     return {'message': 'This is about ODL'}
 
-@app.get("/product")
+@app.get('/product')
 def product():
     return {'message': 'This is a product'}
 
 @app.get("/view")
 def view():
-    
+    data = load_data()
+    return data
+
+@app.get('/products/{id}')
+def viewprod(id: int):
+    data = load_data()
+    for product in data:
+        if product['id'] == id:
+            return product
+    raise HTTPException(status_code=404, detail='product not found')
